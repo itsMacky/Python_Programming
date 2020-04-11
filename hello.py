e73db16be3e804bed39fc2c3c078d7aa5d66c94b
Macky <makrand586@gmail.com>
2020-04-11 21:17:03 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,57 @@
+i = 0 #integer type variables
+s = "hello" # string type variables
+f = 0.0 # float variables
+t = True #boolean
+
+i = i+1 #1
+print(i)
+i += 1 # 2
+print(i)
+i -= 2 #0
+print(i)
+i = 10
+i = i/2.0
+print(i)#5.0
+i = i * 3 #15.0
+print(i)
+
+print(2/2) #1.0 division
+print(2*2) #4.0 multiplication
+print(2 % 2) #2 / 2 -> remainder i.e = 0
+print(3 % 2) #3 / 2 -> remainder i.e = 1
+print(2/2/2) #0.5
+print(2 // 2) #1 integer division
+print(3//2) #1 integer division , round off
+print(3*3)#9.0
+print(3**2)#9.0 -> 3 into the power of 2
+print(3**3)#27.0 -> 3 into the power of 3 -> exponential operators
+
+#Q. ram = 3 appls,shyam = 5 apples, what % of apples does ram and shyam will have respectively
+ram = (3/(3+5)*100)
+print(ram)
+       
+sham = (5/(3+5)*100)
+print(sham)
+
+#ram=3
+#sham=5
+
+#pram=ram/(ram+sham)*100
+#psham=sham/(ram+sham)*100
+#print(ram)
+#print(sham)
+
+a = 3
+b = 2
+
+temp = a
+a=b
+
+
+print(a)
+print(b)
+
+
+
+
+
